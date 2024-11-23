4bfe3fb062cf46d4ad1897a8b034062782832088
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-11-23 14:32:59 -0300

    feat: insert dependencies

--- backend/src/api/config/dependencies.ts
+++ backend/src/api/config/dependencies.ts
@@ -1,12 +1,19 @@
-import { PrismaClient } from '@prisma/client';
-import { ExerciseRepository } from '../repository/ExerciseRepository';
-import { WorkoutRepository } from '../repository/WorkoutRepository';
-import { ExerciseService } from '../services/ExerciseService';
-import { WorkoutService } from '../services/WorkoutService';
+import { PrismaClient } from "@prisma/client";
+import { ExerciseRepository } from "../repository/ExerciseRepository";
+import { WorkoutRepository } from "../repository/WorkoutRepository";
+import { ExerciseService } from "../services/ExerciseService";
+import { WorkoutService } from "../services/WorkoutService";
+import { createWorkout } from "../controllers/workoutController/CreateWorkout";
+import { WorkoutPlanService } from "../services/WorkoutPlanService";
+import { WorkoutPlanRepository } from "../repository/WorkoutPlanRepository";
+import { getWorkout } from "../controllers/workoutController/GetWorkout";
 
 const prisma = new PrismaClient();
 
-const exerciseService = new ExerciseService(new ExerciseRepository());
-const workoutService = new WorkoutService(new WorkoutRepository(prisma));
-
-export const workoutController = new clasaskdf();
+export const exerciseService = new ExerciseService(
+  new ExerciseRepository(prisma)
+);
+export const workoutService = new WorkoutService(new WorkoutRepository(prisma));
+export const workoutPlanService = new WorkoutPlanService(
+  new WorkoutPlanRepository(prisma)
+);
