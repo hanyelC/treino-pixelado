651e680d9bbf56f4616e661ec00f1858348d6a68
boruto <gabrielsantanasantos798@gmail.com>
2024-11-24 12:41:45 -0300

    feat: added the header and a background

--- frontend/src/pages/WorkoutPlan.tsx
+++ frontend/src/pages/WorkoutPlan.tsx
@@ -1,9 +1,36 @@
+import { useNavigate } from "react-router";
+import Header from "../components/Header";
 import { WorkoutCard } from "../components/WorkoutCard";
+import { useState } from "react";
 
 export function WorkoutPlan() {
+  const [activeTab, setActiveTab] = useState("TRAINING");
+  const navigate = useNavigate();
+
+  // Função para alterar o estado ao clicar em um item
+  const handleTabClick = (tab: string) => {
+    setActiveTab(tab);
+    if (tab === "PLAN") navigate("/"); 
+  };
   return (
-    <div className="flex flex-col h-full ">
-      <h1>workout plan</h1>
+    <div className="flex flex-col bg-slate-400 h-full">
+      <Header>
+      <div className="flex flex-col items-center cursor-pointer"
+        onClick={() => handleTabClick("PLAN")}
+      >
+        <p className={`text-lg ${
+                activeTab === "PLAN" ? "text-orange-500" : "text-black"
+              }`}>PLAN</p>
+      </div>
+      <div className="flex flex-col items-center cursor-pointer"
+            onClick={() => handleTabClick("TRAINING")}>
+        <p className={`text-lg ${
+          activeTab === "TRAINING" ? "text-orange-500" : "text-black"
+        }`}>
+          TRAINING
+        </p>
+      </div>
+    </Header>
 
       {workouts.map((w, i) => (
         <WorkoutCard name={w.name} key={i} />
