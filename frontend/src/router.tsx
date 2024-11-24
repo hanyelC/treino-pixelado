a74b266150f418b1031025598aa4fccfbc3d9133
boruto <gabrielsantanasantos798@gmail.com>
2024-11-24 12:23:15 -0300

    feat: added the plan router

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,10 +1,16 @@
 import { createBrowserRouter } from "react-router";
 import { Workout } from "./pages/Workout";
 import { WorkoutPlan } from "./pages/WorkoutPlan";
+import { Plan } from "./pages/Plan";
+
 
 export const router = createBrowserRouter([
   {
     path: "/",
+    element: <Plan />,
+  },
+  {
+    path: "/WorkoutPlan",
     element: <WorkoutPlan />,
   },
   {
