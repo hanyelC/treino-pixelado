39ab0d5d90e224c7e446550313ae84c82404afc1
Allyson Athyrson <allysonathyrson.work@gmail.com>
2024-11-24 14:24:42 -0300

    feat: create Workout screen

--- frontend/src/pages/Workout.tsx
+++ frontend/src/pages/Workout.tsx
@@ -4,8 +4,9 @@ import { WorkoutCardProps } from "../components/WorkoutCard";
 export function Workout() {
   const workoutData = useLocation().state as WorkoutCardProps;
 
+  console.log("workoutData.description", workoutData.description);
   return (
-    <div className="">
+    <div className="h-screen">
       <div className="text-black flex justify-between items-center bg-white px-4">
         <Link
           style={{
@@ -18,23 +19,29 @@ export function Workout() {
         <p className="text-xl">{workoutData.name}</p>
       </div>
 
-      <p className="text-center">{workoutData.description}</p>
+      <div className="p-4 flex flex-col gap-6 bg-gray-200 h-full">
+        <p className="text-center text-black text-lg">
+          {workoutData.description}
+        </p>
 
-      <div className="px-4">
-        {workoutData.exercises.map((exerciseItem) => {
-          return (
-            <div>
-              <div>
-                {/* img */}
+        <div className="px-4 flex flex-col gap-2">
+          {workoutData.exercises.map((exerciseItem) => {
+            return (
+              <div className="flex gap-2 bg-slate-400 p-2 rounded-md text-black">
+                <img
+                  className="w-14 h-w-14"
+                  src={workoutData.imageUrl}
+                  alt="Imagem de exercício"
+                />
 
                 <div>
                   <p>{exerciseItem.name}</p>
-                  <p>Repetições</p>
+                  <p>{exerciseItem.series} Repetições</p>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
